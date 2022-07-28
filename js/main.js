6493f7d4cfdc2e056c72c72f6108b5f78a0e31fc
mohan9211 <81138042+liablemohan@users.noreply.github.com>
2022-07-28 21:44:31 +0530

    presentation Jul 28

--- js/main.js
+++ js/main.js
@@ -305,3 +305,22 @@ $(function(){
 		elm.off("click");
 	});
 });
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementByClass("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= position-sticky) {
+    navbar.classList.add("position-sticky")
+  } else {
+    navbar.classList.remove("position-sticky");
+  }
+} 
